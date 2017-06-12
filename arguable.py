228b816f74bf0394ced894e85939476a6f87cf78
Ian Fisher <ianfisher45@gmail.com>
2017-06-12 15:01:52 -0400

    Add long names for short options

--- arguable.py
+++ arguable.py
@@ -1,10 +1,12 @@
 import sys
 import argparse
 
+
 def parse_args(pattern, args=None):
     """Shortcut for calling parse_args on the object returned by make_parser."""
     args = args if args is not None else sys.argv
     return make_parser(pattern).parse_args(args)
+
 
 def make_parser(pattern):
     """Create an argparse.ArgumentParser object from the argument pattern. The pattern argument
@@ -21,13 +23,32 @@ def make_parser(pattern):
         if token.startswith('--'):
             parser.add_argument(token, action='store_true')
         elif token.startswith('-'):
+            # -xx... where x is the same character is a repeatable option
             if len(token) > 2 and len(set(token[1:])) == 1:
                 parser.add_argument('-' + token[1], action='count', default=0)
+            # -ofv[verbose]... is a series of optional flags, -o -f and -v, where -v has a long
+            # counterpart --verbose
             else:
-                for flag in token[1:]:
-                    parser.add_argument('-' + flag, action='store_true')
+                i = 1
+                while i < len(token):
+                    flag = token[i]
+                    # handle long names
+                    if i < len(token) - 1 and token[i+1] == '[':
+                        end = token.find(']', i + 2)
+                        if end != -1:
+                            long_name = token[i+2:end]
+                            parser.add_argument('-' + flag, '--' + long_name, action='store_true')
+                            i = end + 1
+                        else:
+                            msg = 'long name "{}" needs a terminating "]"'.format(token[i+1:])
+                            raise SyntaxError(msg)
+                    else:
+                        parser.add_argument('-' + flag, action='store_true')
+                        i += 1
+        # foo? is an optional positional argument
         elif token.endswith('?'):
             parser.add_argument(token[:-1], nargs='?')
+        # anything else is interpreted as a required positional argument
         else:
             parser.add_argument(token)
     return parser
--- tests.py
+++ tests.py
@@ -7,9 +7,29 @@ import arguable
 
 class ParserTests(unittest.TestCase):
     def test_long_option(self):
-        parser = arguable.make_parser('--verbose')
-        args = parser.parse_args(['--verbose'])
+        args = arguable.parse_args('--verbose', ['--verbose'])
         self.assertEqual(args.verbose, True)
+
+
+    def test_long_option_syntax(self):
+        args = arguable.parse_args('-v[verbose]fo', ['-v', '-f'])
+        self.assertEqual(args.verbose, True)
+        self.assertEqual(args.f, True)
+        self.assertEqual(args.o, False)
+
+        args = arguable.parse_args('-fv[verbose]o', ['-v', '-f'])
+        self.assertEqual(args.verbose, True)
+        self.assertEqual(args.f, True)
+        self.assertEqual(args.o, False)
+
+        args = arguable.parse_args('-fov[verbose]', ['-v', '-f'])
+        self.assertEqual(args.verbose, True)
+        self.assertEqual(args.f, True)
+        self.assertEqual(args.o, False)
+
+        with self.assertRaises(SyntaxError):
+            # forget the ending ']'
+            arguable.make_parser('-fov[verbose')
 
 
     def test_repeatable(self):
