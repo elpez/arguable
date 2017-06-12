a45b4c0c5dcc516b1f2b83b0513454eaa39e7e15
Ian Fisher <ianfisher45@gmail.com>
2017-06-12 14:24:51 -0400

    Remove stale code

--- arguable.py
+++ arguable.py
@@ -1,19 +1,6 @@
 import sys
 import argparse
 
-class ArgumentParser(argparse.ArgumentParser):
-    def add_required_arg(self, name):
-        self.add_argument(name)
-
-    def add_optional_arg(self, name, default=None):
-        self.add_argument(name, nargs='?', default=default)
-
-    def add_flag(self, name, repeat=False, dest=None):
-        if repeat:
-            self.add_argument(name, action='count', dest=dest)
-        else:
-            self.add_argument(name, action='store_true', dest=dest)
-
 def parse_args(pattern, args=None):
     args = args if args is not None else sys.argv
     return make_parser(pattern).parse_args(args)
--- tests.py
+++ tests.py
@@ -6,16 +6,6 @@ import arguable
 
 
 class ParserTests(unittest.TestCase):
-    def test_basic(self):
-        parser = arguable.ArgumentParser(description='Process some files')
-        parser.add_required_arg('infile')
-        parser.add_optional_arg('outfile', default='out.xml')
-        parser.add_flag('-v', repeat=True, dest='verbosity')
-        args = parser.parse_args(['-vvv', 'input.xml'])
-        self.assertEqual(args.verbosity, 3)
-        self.assertEqual(args.infile, 'input.xml')
-        self.assertEqual(args.outfile, 'out.xml')
-
     def test_parse_args(self):
         parser = arguable.make_parser('-v infile outfile?')
         args = parser.parse_args(['test.xml', '-v'])
